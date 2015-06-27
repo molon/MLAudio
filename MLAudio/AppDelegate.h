ca5a4d38eef9edd3f93e8f6231da320205ed599d
molon <dudl@qq.com>
2015-06-28 06:00:26 +0800

    Initial Commit

---
+++ MLAudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLAudio
+//
+//  Created by molon on 15/6/28.
+//  Copyright (c) 2015年 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLAudio
+//
+//  Created by molon on 15/6/28.
+//  Copyright (c) 2015年 molon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
